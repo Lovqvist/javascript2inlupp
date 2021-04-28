40f750c4f0d1a03a81fad0d1b5b0d4e864142411
Lovqvist <sandra.lovqvist@gmail.com>
2021-04-28 09:19:31 +0200

    first commit

---
+++ app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <navbar />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'E-commersical',
+  components: { 
+    Navbar
+  },
+  
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ app/src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-info">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button
+            class="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            >
+            <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link" href="#">Products</a>
+                </div>
+            </div>
+            <a class="nav-link" href="#">
+                <i class="fas fa-shopping-cart"></i>
+            </a>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+
+</style>
